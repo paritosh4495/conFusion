68137c62cdaece8e169545edd6539820e73991dd
Paritosh Pal <paritoshpal7796@gmail.com>
2024-10-17 18:34:34 +0530

    Express REST API With Mongoose Part 1

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -11,11 +12,27 @@ var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+
+const connect = mongoose.connect(url);
+
+connect.then((db) =>{
+  console.log('Connected Correctly to the DB SERVER');
+},(err)=>{
+  console.log(err);
+});
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,48 +1,81 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Dishes = require('../models/dishes');
+
+
 
 const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-.all((req,res,next) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-})
 .get((req,res, next) =>{
-    res.end('Will Send All the Dishes to You!')
+    Dishes.find({})
+        .then((dishes)=> {
+            res.status = 200;
+            res.setHeader('Content-Type','application/json');
+            res.json(dishes);
+        },(err)=> next(errr))
+        .catch((err)=>next(err));   
 })
 .post((req,res, next)=>{
-    res.end('Will Add the dish: '+req.body.name + " with details :  "+req.body.description);
+  Dishes.create(req.body)
+  .then((dish)=>{
+    console.log('Dish Created : ',dish);
+    res.status = 200;
+    res.setHeader('Content-Type','application/json');
+    res.json(dish);
+  },(err)=> next(errr))
+  .catch((err)=>next(err)); 
 })
 .put((req,res, next)=>{
     res.statusCode = 403;
     res.end("PUT Operation not supported on /dishes ");
 })
 .delete((req,res, next) =>{
-    res.end('Delething  all the dishes!')
+    Dishes.deleteMany({})
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    },(err)=> next(errr))
+    .catch((err)=>next(err));
 });
 
 dishRouter.route('/:dishId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
 .get((req, res, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+   Dishes.findById(req.params.dishId)
+   .then((dish)=>{
+    res.status = 200;
+    res.setHeader('Content-Type','application/json');
+    res.json(dish);
+  },(err)=> next(errr))
+  .catch((err)=>next(err)); 
 })
 .post((req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /dishes/' + req.params.dishId);
 })
 .put((req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+   Dishes.findByIdAndUpdate(req.params.dishId, {
+    $set: req.body
+   }, {new : true})
+   .then((dish)=>{
+    res.status = 200;
+    res.setHeader('Content-Type','application/json');
+    res.json(dish);
+  },(err)=> next(errr))
+  .catch((err)=>next(err)); 
 })
 .delete((req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
+  Dishes.findByIdAndDelete(req.params.dishId)
+  .then((resp)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','application/json');
+    res.json(resp);
+},(err)=> next(errr))
+.catch((err)=>next(err));
 });
 
 module.exports = dishRouter;
