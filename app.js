d9b9d5b5567026fe3a0571a03b9e34f2fa95dea3
Paritosh Pal <paritoshpal7796@gmail.com>
2024-10-18 12:23:07 +0530

    Basic Authentication

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -38,6 +39,39 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth(req,res, next){
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+  
+  if(!authHeader){
+    var err = new Error('You are not Authenticated! ');
+    
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+  var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
+
+  var username = auth[0];
+  var password = auth[1];
+
+  if(username === 'admin' && password === 'password'){
+    next();
+  }
+  else{
+    var err = new Error('You are not Authenticated! ');
+    
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
