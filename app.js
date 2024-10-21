03d13c570cbd1f87b2465c14c8e95fdf599e2feb
Paritosh Pal <paritoshpal7796@gmail.com>
2024-10-21 12:28:38 +0530

    Passport

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
-
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 
 var indexRouter = require('./routes/index');
@@ -41,6 +42,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
+
 app.use(session({
   name : 'session-id',
   secret : '12345-67890-09876-54321',
@@ -49,26 +51,21 @@ app.use(session({
   store : new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req,res, next){
-  console.log(req.session);
-
-  if(!req.session.user){
+  console.log(req.user);
+  if(!req.user){
       var err = new Error('You are not Authenticated! ');
       err.status = 403;
       return next(err);
   }
   else{
-    if(req.session.user === 'authenticated'){
       next();
-    }
-    else{
-      var err = new Error('You are not Authenticated! ');
-      err.status = 403;
-      return next(err);
-    }
   }
  
 }
